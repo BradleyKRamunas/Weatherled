cd9825afd944d408b72052c8a1c5600016bb66a5
Bradley Ramunas <bradleyr@stanford.edu>
2019-11-07 17:06:45 -0800

    cleaned up and added serial_write.py

--- weather.py
+++ weather.py
@@ -1,12 +1,11 @@
 import requests
 import json
-import pprint
 from weather_provider import WeatherProvider
 from weather_condition import WeatherCondition
 from mock_weather_provider import MockWeatherProvider
 from bitmaps import convert_condition_to_int, convert_temperature_to_int
 
-weather_provider = MockWeatherProvider()
+weather_provider = WeatherProvider()
 condition, temp = weather_provider.get_current_weather()
 print(condition, temp)
 print(convert_condition_to_int(condition), convert_temperature_to_int(temp))
